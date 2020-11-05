839274f3b6717d564cabae2bab8067339c02d9ba
cega <cgalindez@gmail.com>
2020-11-05 20:38:24 -0300

    1 file added article.effect

---
+++ src/app/effects/article.effects.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import * as fromActions from '../actions/article.actions';
+import { ArticleService } from '../services/article.service';
+
+@Injectable()
+export class ArticleEffects {
+
+  constructor(
+    private actions$: Actions,
+    private articleService: ArticleService
+  ) {}      
+
+  loadAllArticles$ = createEffect(() => this.actions$.pipe(
+    ofType(fromActions.ArticleActionTypes.LOAD_ALL_ARTICLES),    
+    switchMap(() => this.articleService.getAllArticles().pipe(
+      map(data => ({
+        type: fromActions.ArticleActionTypes.LoadArticlesSuccess,
+        payload: data
+      }))
+    ))
+  ))
+  // @Effect() 
+  // loadAllArticles$: Observable<Action> = this.actions$.pipe(
+  //     ofType(fromActions.ArticleActionTypes.LOAD_ALL_ARTICLES),
+  //     switchMap(() => this.articleService.getAllArticles(),
+  //     map(data => new fromActions.LoadArticlesSuccess(
+  //       { articles: data }
+  //     ))
+  //   )
+}
+
