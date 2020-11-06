816bd14ada8a8e9aa1fe856df6b1c48d2d9f4a0e
cega <cgalindez@gmail.com>
2020-11-06 10:50:53 -0300

    2 files modified working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  template: `
+		<app-article></app-article>
+     `
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
